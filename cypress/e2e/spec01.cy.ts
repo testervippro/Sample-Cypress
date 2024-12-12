117c73dc398c36768a1b68716717bcd0cb993be4
cuxuanthoai <cuxuanthoai@gmail.com>
2024-12-12 23:20:50 +0700

    add cmd execute powershell

--- cypress/e2e/spec01.cy.ts
+++ cypress/e2e/spec01.cy.ts
@@ -1,11 +1,15 @@
 describe("Connect to database", () => {
-
   const query = "";
 
   it("can call a database and execute a query", () => {
-    cy.sqlServer(query).then ((result :any)=> {
-      cy.log(result.recordset[0].JobId)
-    })
+    cy.sqlServer(query).then((result: any) => {
+      cy.log(result.recordset[0].JobId);
+    });
+  });
 
+  it("test can ps1 file", () => {
+    cy.executePowerShell("path/to/your/script.ps1").then((result) => {
+      console.log(result.stdout);
+    });
   });
 });
