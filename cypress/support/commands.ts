bc34209b2cfde7d700bd079868912d6f69c2f416
testervippro <92687973+testervippro@users.noreply.github.com>
2024-12-28 20:47:19 +0700

    add handle create policy microsoft defender

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 
+import { use } from "chai";
 import { userClient } from "../graph/auth";
 
 // ***********************************************
@@ -28,59 +29,98 @@ import { userClient } from "../graph/auth";
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
-
-// declare
+export{}
+// declare 
 declare global {
   namespace Cypress {
     interface Chainable {
-      sqlServer(query: string): Chainable<any>;
-      getAPI(url: string): Chainable<any>;
-      updateAPI(url: string, body: any): Chainable<any>;
-      selectProduct(query: string): Chainable<any>;
-      executePowerShell(path: string): Chainable<any>;
+        sqlServer(query: string): Chainable<any>;
+        getAPI(url: string): Chainable<any>;
+        updateAPI(url: string, body: any): Chainable<any>;
+        getMicrosoftDenfenderPolicy( path :string):Chainable<any>;
+        setMicrosoftDenfenderPolicy( path :string):Chainable<any>;
     }
   }
 }
 
-Cypress.Commands.add("executePowerShell", (scriptPath) => {
-  return cy
-    .exec(`powershell -File ${scriptPath}`, { failOnNonZeroExit: false })
+// Cypress.Commands.add('sqlServer', (query: string) => {
+//     if (!query) {
+//       throw new Error('Query must be set');
+//   }
+  
+//     return cy.task('queryDatabase', query).then((response: unknown) => {
+//       let result: any[] = []; 
+
+//       const typedResponse = response as SqlResponse[];
+  
+//       const flatten = (r: any): any => Array.isArray(r) && r.length === 1 ? flatten(r[0]) : r;
+  
+//       if (typedResponse) {
+//         for (let i in typedResponse) {
+//           result[i] = [];
+//           for (let c in typedResponse[i]) {
+//             result[i][c] = typedResponse[i][c].value; 
+//           }
+//         }
+//         result = flatten(result);
+//       } else {
+//         result = typedResponse; 
+//       }
+  
+//       return result;
+//     });
+//   });
+
+Cypress.Commands.add('sqlServer', (query: string) => {
+  if (!query) {
+    throw new Error('Query must be set');
+}
+  return cy.task('queryDatabase', query)
+});
+
+//get API
+Cypress.Commands.add('getAPI', (url: string) => {
+  if (!url) {
+    throw new Error('Query must be set');
+}
+  return cy.task('getAPI', url)
+});
+
+//update API
+Cypress.Commands.add('updateAPI', (url: string, body:any) => {
+  if (!url) {
+    throw new Error('Query must be set');
+}
+  return cy.task('updateAPI',{ url,body})
+});
+
+
+
+
+Cypress.Commands.add('getMicrosoftDenfenderPolicy', (setScriptPolicyPath) => {
+  return cy.exec(`powershell -File "cypress\\defender\\setPolicy.ps1" -username "ad.onmicrosoft.com" -password "Demo" -command ${setScriptPolicyPath}`, { failOnNonZeroExit: false })
     .then((result) => {
       return {
         code: result.code,
         stdout: result.stdout.trim(),
-        stderr: result.stderr.trim(),
+        stderr: result.stderr.trim()
+      };
+    });
+});
+//call setPolicy.ps1 then setPolicy.ps1 call setScriptPolicyPath to execute  
+Cypress.Commands.add('setMicrosoftDenfenderPolicy', (setScriptPolicyPath) => {
+  const _sciptSetPolicy = "cypress\defender\setPolicy.ps1"
+  return cy.exec(`powershell -File cypress\\defender\\setPolicy.ps1 -username "lukecom" -password "Demo" -command ${setScriptPolicyPath}`, { failOnNonZeroExit: false })
+    .then((result) => {
+      return {
+        code: result.code,
+        stdout: result.stdout.trim(),
+        stderr: result.stderr.trim()
       };
     });
 });
 
-Cypress.Commands.add("selectProduct", (productName) => {
-  cy.get("h4.card-title").each(($el, index, $list) => {
-    if ($el.text().includes(productName)) {
-      cy.get("button.btn.btn-info").eq(index).click();
-    }
-  });
-});
 
-Cypress.Commands.add("sqlServer", (query: string) => {
-  if (!query) {
-    throw new Error("Query must be set");
-  }
-  return cy.task("queryDatabase", query);
-});
 
-//get API
-Cypress.Commands.add("getAPI", (url: string) => {
-  if (!url) {
-    throw new Error("Query must be set");
-  }
-  return cy.task("getAPI", url);
-});
 
-//update API
-Cypress.Commands.add("updateAPI", (url: string, body: any) => {
-  if (!url) {
-    throw new Error("Query must be set");
-  }
-  return cy.task("updateAPI", { url, body });
-});
+  
