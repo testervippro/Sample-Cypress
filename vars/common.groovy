8bd6d8beebfea195fd5d31ae0055e1c24c1f8d07
cuxuanthoai <cuxuanthoai@gmail.com>
2025-01-29 19:37:45 +0700

    make send email , telegram to function

--- vars/common.groovy
+++
@@ -1,8 +0,0 @@
-// vars/common.groovy
-def greet(String name) {
-    echo "Hello, ${name}! This is a custom greeting from the Groovy script."
-}
-
-def farewell(String name) {
-    echo "Goodbye, ${name}! This is a custom farewell from the Groovy script."
-}
