cd6b58b212cd850f2aec841a52cd9fe1f5cbbcdd
cuxuanthoai <cuxuanthoai@gmail.com>
2025-02-01 17:49:15 +0700

    add gitlab cicd use cypress split and merge report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,65 +1,31 @@
 import { defineConfig } from "cypress";
-//import * as auth from "./cypress/graph/auth.ts";
-
-
-// async function getAPI(url: string) {
-//   try {
-//     const result = await auth.userClient.api(url).get();
-//     return result;
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// }
-
-// async function updateAPI(url: string, body: any) {
-//   let _body = JSON.stringify(body);
-
-//   try {
-//     const result = await auth.userClient.api(url).update(_body);
-//     return null;
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// }
 
 export default defineConfig({
   projectId: 'jz2xs6', // A
   env: {},
-    reporter: 'cypress-multi-reporters',
-    reporterOptions: {
-      reporterEnabled: 'mochawesome,mocha-junit-reporter',
-      mochawesomeReporterOptions: {
-        reportDir: 'cypress/reports/mochawesome-report',   // Directory for Mochawesome reports
-        reportFilename: 'report',                          // File name for the Mochawesome report
-        overwrite: false,                                  // Do not overwrite the report
-        html: false,                                       // Disable HTML report generation
-        json: true                                         // Enable JSON report generation
-      },
-      mochaJunitReporterOptions: {
-        mochaFile: 'cypress/reports/junit/test-results.xml' // Path to the JUnit XML file
-      }
-    },
+  reporter: 'mochawesome', // Use only Mochawesome reporter
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome-report', // Directory for Mochawesome reports
+    reportFilename: 'report',                       // File name for the Mochawesome report
+    overwrite: false,                               // Do not overwrite the report
+    html: true,                                     // Enable HTML report generation
+    json: true                                      // Enable JSON report generation
+  },
+  // https://github.com/adamgruber/mochawesome when use cypres split 
+  // reporter: 'mochawesome',
+  // reporterOptions: {
+  //   useInlineDiffs: true,
+  //   embeddedScreenshots: true,
+  //   reportDir: 'cypress/results',
+  //   reportFilename: '[name].html',
+  //   overwrite: true,
+  //   html: true,
+  //   json: true,
+  // },
+
   e2e: {
     setupNodeEvents(on, config) {
-      //sql db
-      
-
-
-      // //get  api
-      // on("task", {
-      //   getAPI(url) {
-      //     return getAPI(url);
-      //   },
-      // });
-
-      // //update api
-      // on("task", {
-      //   updateAPI({ url, body }) {
-      //     return updateAPI(url, body);
-      //   },
-      // });
-
-   
+      // Add any setup or custom tasks here
 
       return config;
     },
